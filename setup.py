be94d4393a650829d588dc6e5cca2bd303428aa2
Dylan <dylanwal@mit.edu>
2021-07-09 10:54:10 -0400

    "v0.0.1"

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='rxnpy',
     version='0.0.1',
-    packages=[''],
     url='https://github.com/dylanwal/rxnpy',
-    license='MIT License ',
     author='Dylan',
     author_email='',
-    description='Chemistry Stuff'
+    description='Chemistry stuff',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "rxnpy"},
+    packages=setuptools.find_packages(where="rxnpy"),
+    python_requires=">=3.6",
 )
